ce7aaa7982f690fb1e21ca7272d560ea5d794461
amarin <asmadews@gmail.com>
2020-05-18 12:25:48 +0300

    add constants

---
+++ constants.go
@@ -0,0 +1,13 @@
+package binutils
+
+// Some useful constants
+const (
+	Int64size  = 8 // int64 size in bytes
+	Uint64size = 8 // uint64 size in bytes
+	Unt32size  = 4 // int32 size in bytes
+	Uint32size = 4 // uint32 size in bytes
+	Int16size  = 2 // int16 size in bytes
+	Uint16size = 2 // uint16 size in bytes
+	Int8size   = 1 // int8 size in bytes
+	Uint8size  = 1 // uint8 size in bytes
+)

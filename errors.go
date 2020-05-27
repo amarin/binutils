dd5e6782f964e93b48c235b14a55d508f8ca489a
amarin <asmadews@gmail.com>
2020-05-28 01:40:02 +0300

    add WrapError constructor and fix some linter warnings

--- errors.go
+++ errors.go
@@ -6,15 +6,20 @@ import (
 
 // Error type implements error interface and used in package routines.
 type Error struct {
-	formattedString string
+	wrapped     error
+	description string
 }
 
 // Error returns formatted error string. Implements error interface.
 func (e Error) Error() string {
-	return e.formattedString
+	if e.wrapped != nil {
+		return e.description + ": " + e.wrapped.Error()
+	}
+
+	return e.description
 }
 
-// GoString returns formatted error string prefixes with package name and type. Implements GoStringer
+// GoString returns formatted error string prefixes with package name and type. Implements GoStringer.
 func (e Error) GoString() string {
 	return "binutils.Error:" + e.Error()
 }
@@ -22,5 +27,10 @@ func (e Error) GoString() string {
 // NewError is a default error constructor.
 // Takes format and arguments to make error description using fmt.Sprintf() formatting rules.
 func NewError(format string, args ...interface{}) error {
-	return &Error{fmt.Sprintf(format, args...)}
+	return &Error{description: fmt.Sprintf(format, args...)}
+}
+
+// WrapError creates new error, wrapping underlying error with formatted string.
+func WrapError(wrap error, format string, args ...interface{}) *Error {
+	return &Error{description: fmt.Sprintf(format, args...), wrapped: wrap}
 }

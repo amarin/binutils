643df2c900f5a021567e1c60ea993415042810b0
amarin <asmadews@gmail.com>
2020-05-28 02:30:51 +0300

    add empty test cases for LoadBinary & SaveBinary

--- load_save.go
+++ load_save.go
@@ -6,28 +6,40 @@ import (
 	"path/filepath"
 )
 
-// SaveBinary saves binary data of encoding.BinaryMarshaler implementing object into specified file
+// SaveBinary saves binary data of encoding.BinaryMarshaler implementing object into specified file.
 func SaveBinary(filename string, dict encoding.BinaryMarshaler) error {
-	if absFileName, err := filepath.Abs(filename); err != nil {
-		return err
-	} else if writer, err := os.Create(absFileName); err != nil {
-		return err
-	} else if data, err := dict.MarshalBinary(); err != nil {
-		return err
-	} else if written, err := writer.Write(data); err != nil {
-		return err
-	} else if written != len(data) {
-		return NewError("written only %d bytes of %d", written, len(data))
-	} else {
-		return nil
+	absFileName, err := filepath.Abs(filename)
+	if err != nil {
+		return WrapError(err, "cant detect absolute path")
 	}
+	// prepare writer
+	writer, err := os.Create(absFileName)
+	if err != nil {
+		return WrapError(err, "cant open file for writing")
+	}
+	// marshal data
+	data, err := dict.MarshalBinary()
+	if err != nil {
+		return WrapError(err, "cant marshal data")
+	}
+	// write data
+	written, err := writer.Write(data)
+	if err != nil {
+		return WrapError(err, "cant write data")
+	}
+	// check all data written
+	if written != len(data) {
+		return NewError("written only %d bytes of %d", written, len(data))
+	}
+	// return ok
+	return nil
 }
 
 // LoadBinary adds binary data from specified file into target BufferUnmarshaler implementing object
 // Returns error if any file path resolution problem or file data empty or some binary data was not decoded.
 func LoadBinary(filename string, dict BufferUnmarshaler) error {
 	buffer := NewBuffer([]byte{})
-	if bytesLoaded, err := buffer.WriteFromFile(filename); err != nil {
+	if bytesLoaded, err := buffer.LoadFromFilePath(filename); err != nil {
 		return err
 	} else if bytesLoaded == 0 {
 		return NewError("loaded %d bytes, cant unmarshal", bytesLoaded)
@@ -36,5 +48,6 @@ func LoadBinary(filename string, dict BufferUnmarshaler) error {
 	} else if buffer.Len() != 0 {
 		return NewError("consumed %d bytes, %d bytes rest", bytesLoaded-buffer.Len(), buffer.Len())
 	}
+
 	return nil
 }
---
+++ load_save_test.go
@@ -0,0 +1,42 @@
+package binutils_test
+
+import (
+	"encoding"
+	"testing"
+
+	"github.com/amarin/binutils"
+)
+
+func TestLoadBinary(t *testing.T) {
+	for _, tt := range []struct {
+		name    string
+		dict    binutils.BufferUnmarshaler
+		wantErr bool
+	}{} {
+		tt := tt // pin tt
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt // pin tt again
+			filename := ""
+			if err := binutils.LoadBinary(filename, tt.dict); (err != nil) != tt.wantErr {
+				t.Errorf("LoadBinary() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestSaveBinary(t *testing.T) {
+	for _, tt := range []struct {
+		name     string
+		filename string
+		dict     encoding.BinaryMarshaler
+		wantErr  bool
+	}{} {
+		tt := tt // pin tt
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt // pin tt again
+			if err := binutils.SaveBinary(tt.filename, tt.dict); (err != nil) != tt.wantErr {
+				t.Errorf("SaveBinary() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}

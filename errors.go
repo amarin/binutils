0f019940d8c35fee16d717b75eafd518e6a37cb1
amarin <asmadews@gmail.com>
2020-07-15 12:17:52 +0300

    Add descriptive comments to predefined errors

--- errors.go
+++ errors.go
@@ -4,19 +4,35 @@ import (
 	"errors"
 )
 
+// Some predefined errors used during processing.
+
 var (
-	ErrExpected1   = errors.New("expected 1 byte")              // expected exactly 1 byte
-	ErrExpected2   = errors.New("expected 2 bytes")             // expected exactly 2 bytes
-	ErrExpected4   = errors.New("expected 4 bytes")             // expected exactly 4 bytes
-	ErrExpected8   = errors.New("expected 8 bytes")             // expected exactly 8 bytes
-	ErrMinimum1    = errors.New("at least 1 byte required")     // expected at least 1 byte
-	ErrRequired0T  = errors.New("required 0-terminated string") // expected 0-byte termination
-	ErrSizing      = errors.New("unexpected sizing")            // sizing unexpected
-	ErrNegativeLen = errors.New("negative length")              // negative length
-	ErrOverflowBy  = errors.New("type overflow")                // value overflows type
-	ErrBuffer      = errors.New("buffer")                       // general buffer error
-	ErrDecodeTo    = errors.New("decode")                       // decode error
-	ErrMissedData  = errors.New("insufficient bytes")           // some bytes missed
-	ErrExtraData   = errors.New("extra bytes")                  // extra bytes after decoding means error
-	ErrRead        = errors.New("read")                         // general read error
+	// ErrExpected1 returned if expected exactly 1 byte
+	ErrExpected1 = errors.New("expected 1 byte")
+	// ErrExpected2 returned if expected exactly 2 bytes
+	ErrExpected2 = errors.New("expected 2 bytes")
+	// ErrExpected4 returned if expected exactly 4 bytes
+	ErrExpected4 = errors.New("expected 4 bytes")
+	// ErrExpected8 returned if expected exactly 8 bytes
+	ErrExpected8 = errors.New("expected 8 bytes")
+	// ErrMinimum1 returned if expected at least 1 byte
+	ErrMinimum1 = errors.New("at least 1 byte required")
+	// ErrRequired0T returned if expected 0-byte termination
+	ErrRequired0T = errors.New("required 0-terminated string")
+	// ErrSizing returned if sizing unexpected
+	ErrSizing = errors.New("unexpected sizing")
+	// ErrNegativeLen returned if negative length
+	ErrNegativeLen = errors.New("negative length")
+	// ErrOverflowBy returned if value overflows type
+	ErrOverflowBy = errors.New("type overflow")
+	// ErrBuffer returned if general buffer error
+	ErrBuffer = errors.New("buffer")
+	// ErrDecodeTo returned if decode error
+	ErrDecodeTo = errors.New("decode")
+	// ErrMissedData returned if some bytes missed
+	ErrMissedData = errors.New("insufficient bytes")
+	// ErrExtraData returned if extra bytes after decoding means error
+	ErrExtraData = errors.New("extra bytes")
+	// ErrRead returned if general read error
+	ErrRead = errors.New("read")
 )

45a91b11e2cce98fdfa273ac3192afe4b3eb7a77
amarin <asmadews@gmail.com>
2020-07-15 11:18:42 +0300

    add predefined errors to prevent runtime error creation

--- errors.go
+++ errors.go
@@ -1,7 +1,17 @@
 package binutils
 
 import (
+	"errors"
 	"fmt"
+)
+
+var (
+	ErrExpected1byte                = errors.New("expected 1 byte")
+	ErrExpected2bytes               = errors.New("expected 2 bytes")
+	ErrExpected4bytes               = errors.New("expected 4 bytes")
+	ErrExpected8bytes               = errors.New("expected 8 bytes")
+	ErrExpectedAtLeast1Byte         = errors.New("at least 1 byte required")
+	ErrRequiredZeroTerminatedString = errors.New("required 0-terminated sring")
 )
 
 // Error type implements error interface and used in package routines.

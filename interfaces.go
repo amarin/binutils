4ed28b0e744b21b74325780e1c7a5122cbd6d255
Aleksey Marin <alexey.marin@media-tel.ru>
2022-05-05 15:33:51 +0300

    added Binary type marshaller's interfaces to use with BinaryMarshaller.WriteObject

--- interfaces.go
+++ interfaces.go
@@ -1,6 +1,6 @@
 package binutils
 
-// BufferUnmarshaler defines interface to objects able to unmarshal itself from binary buffer.
+// BufferUnmarshaler requires interface implementations provides method to unmarshal itself from Buffer.
 // Such objects should read only own bytes from buffer leaving extra bytes intact for others.
 type BufferUnmarshaler interface {
 	UnmarshalFromBuffer(*Buffer) error
@@ -16,20 +16,60 @@ type untilStopByteReader interface {
 	ReadBytes(stopByte byte) ([]byte, error)
 }
 
-// BinaryReaderFrom is the interface that wraps the BinaryReadFrom method.
-//
-// BinaryReadFrom reads implementation data from binary until its data restored or any error encountered.
+// BinaryReaderFrom interface wraps the BinaryReadFrom method.
+// Implementation method BinaryReadFrom reads implementors data from BinaryReader
+// until its data restored or any error encountered.
 // The return value n is the number of bytes taken from reader.
 // Any error except io.EOF encountered during the read is also returned.
 type BinaryReaderFrom interface {
 	BinaryReadFrom(*BinaryReader) (n int64, err error)
 }
 
-// BinaryWriterTo is the interface that wraps the BinaryWriteTo method.
-//
-// BinaryWriteTo writes implementors data into writer until marshalled or any error occurs.
-// The return value n is the number of bytes
-// written. Any error encountered during writing is also returned.
+// BinaryWriterTo interface wraps the BinaryWriteTo method.
+// Implementation method BinaryWriteTo writes implementors data into BinaryWriter
+// until all marshalled or any error occurs.
+// The return value n is the number of bytes written into BinaryWriter.
+// Any error encountered during writing is also returned.
 type BinaryWriterTo interface {
 	BinaryWriteTo(*BinaryWriter) (n int64, err error)
 }
+
+// BinaryUint8 requires implementation could translate its value to uint8 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryUint8 interface{ Uint8() uint8 }
+
+// BinaryUint16 requires implementation could translate its value to uint16 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryUint16 interface{ Uint16() uint16 }
+
+// BinaryUint32 requires implementation could translate its value to uint32 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryUint32 interface{ Uint32() uint32 }
+
+// BinaryUint64 requires implementation could translate its value to uint64 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryUint64 interface{ Uint64() uint64 }
+
+// BinaryInt8 requires implementation could translate its value to int8 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryInt8 interface{ Int8() int8 }
+
+// BinaryInt16 requires implementation could translate its value to int16 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryInt16 interface{ Int16() int16 }
+
+// BinaryInt32 requires implementation could translate its value to int32 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryInt32 interface{ Int32() int32 }
+
+// BinaryInt64 requires implementation could translate its value to int64 type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryInt64 interface{ Int64() int64 }
+
+// BinaryRune requires implementation could translate its value to rune type.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryRune interface{ Rune() rune }
+
+// BinaryString requires implementation could translate its value to string suitable to serialize as binary.
+// Used in unified BinaryWriter.WriteObject method.
+type BinaryString interface{ BinaryString() string }

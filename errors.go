44d7ad64183404bad05cdf65ee2b97f297d68a2f
Aleksey Marin <alexey.marin@media-tel.ru>
2022-05-12 00:19:34 +0300

    all used errors derived from base binutils.Error

--- errors.go
+++ errors.go
@@ -2,55 +2,41 @@ package binutils
 
 import (
 	"errors"
+	"fmt"
 )
 
 // Some predefined errors used during processing.
 var (
+	// Error indicates any binutils errors.
+	Error = errors.New("binutils")
+
+	// ErrNilPointer indicates nil pointer received when required valid pointer of specified type.
+	ErrNilPointer = fmt.Errorf("%w: nill pointer", Error)
+
 	// ErrExpected1 returned if expected exactly 1 byte.
-	ErrExpected1 = errors.New("expected 1 byte")
+	ErrExpected1 = fmt.Errorf("%w: expected 1 byte", Error)
 
 	// ErrExpected2 returned if expected exactly 2 bytes.
-	ErrExpected2 = errors.New("expected 2 bytes")
+	ErrExpected2 = fmt.Errorf("%w: expected 2 bytes", Error)
 
 	// ErrExpected4 returned if expected exactly 4 bytes.
-	ErrExpected4 = errors.New("expected 4 bytes")
+	ErrExpected4 = fmt.Errorf("%w: expected 4 bytes", Error)
 
 	// ErrExpected8 returned if expected exactly 8 bytes.
-	ErrExpected8 = errors.New("expected 8 bytes")
+	ErrExpected8 = fmt.Errorf("%w: expected 8 bytes", Error)
 
 	// ErrMinimum1 returned if expected at least 1 byte.
-	ErrMinimum1 = errors.New("at least 1 byte required")
+	ErrMinimum1 = fmt.Errorf("%w: at least 1 byte required", Error)
 
 	// ErrRequired0T returned if expected 0-byte termination.
-	ErrRequired0T = errors.New("required 0-terminated string")
-
-	// ErrSizing returned if sizing unexpected.
-	ErrSizing = errors.New("unexpected sizing")
-
-	// ErrNegativeLen returned if negative length.
-	ErrNegativeLen = errors.New("negative length")
-
-	// ErrOverflowBy returned if value overflows type.
-	ErrOverflowBy = errors.New("type overflow")
-
-	// ErrBuffer returned if general buffer error.
-	ErrBuffer = errors.New("buffer")
+	ErrRequired0T = fmt.Errorf("%w: required 0-terminated string", Error)
 
 	// ErrDecodeTo returned if decode error.
-	ErrDecodeTo = errors.New("decode")
-
-	// ErrMissedData returned if some bytes missed.
-	ErrMissedData = errors.New("insufficient bytes")
-
-	// ErrExtraData returned if extra bytes after decoding means error.
-	ErrExtraData = errors.New("extra bytes")
+	ErrDecodeTo = fmt.Errorf("%w: decode", Error)
 
 	// ErrRead returned if general read error.
-	ErrRead = errors.New("read")
-
-	// ErrWrite returned if general write error.
-	ErrWrite = errors.New("write")
+	ErrRead = fmt.Errorf("%w: read", Error)
 
 	// ErrClose returned if general close error.
-	ErrClose = errors.New("close")
+	ErrClose = fmt.Errorf("%w: close", Error)
 )
